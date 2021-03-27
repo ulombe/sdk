1c2b711868993e4ff0109e8daee638ebe33e8bf7
Edson Michaque <edson@michaque.com>
2021-03-27 09:19:27 +0200

    Improve unit tests for Change type

--- change_test.go
+++ change_test.go
@@ -1,6 +1,7 @@
 package sdk
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -27,25 +28,47 @@ func TestChange_String(t *testing.T) {
 				"name": "John",
 				"age": 29,
 				"tall": true,
+				"fullName": map[string]string{
+					"first": "John",
+					"last": "Doe",
+				},
 			},
 			expected: map[string]interface{} {
 				"name": "John",
 				"age": 29,
 				"tall": true,
+				"fullName": map[string]string{
+					"first": "John",
+					"last": "Doe",
+				},
 			},
 		},
+		{
+			name: "Create User#3",
+			got: AttributeValueMap {
+				"name": "John",
+				"age": 29,
+				"tall": nil,
+			},
+			expected: map[string]interface{} {
+				"name": "John",
+				"age": 29,
+				"tall": nil,
+			},
+		},
+
 	}
 
 	for _, test := range tt {
 		t.Run(test.name, func(t *testing.T) {
 			newChange := NewChange(test.got)
 
-			for k, v := range test.got {
+			for k, _ := range test.got {
 				if attr, err := newChange.Get(k); err != nil {
 					t.Errorf("Didn't find a value for %s key", k)
 				} else {
-					if attr != test.expected[k] {
-						t.Errorf("Expected %v for attribute %v but found %v", test.expected[k], v, attr)
+					if !reflect.DeepEqual(attr, test.expected[k]) {
+						t.Errorf("Expected %v for attribute %v but found %v", test.expected[k], k, attr)
 					}
 				}
 			}
